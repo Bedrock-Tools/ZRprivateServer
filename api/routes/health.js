b18988f29465e41329817981b394f60b1eda8bd2
JamzOhJamz <87213748+JamzOhJamz@users.noreply.github.com>
2023-07-10 13:55:55 -0600

    remove
    thing

--- api/routes/health.js
+++ api/routes/health.js
@@ -3,11 +3,7 @@ export const prefix = '/api';
 async function routes(app) {
     app.get('/', () => {
         return {
-            status: 'success',
-            gateway: {
-                url: process.env.MASON_ENDPOINT,
-                useHttps: process.env.MASON_USE_HTTPS?.toLowerCase() === 'true'
-            }
+            status: 'success'
         };
     });
 }
