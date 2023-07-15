d7fb2f6caac4bb5ac41339283cddb4c1b1878dde
JamzOhJamz <87213748+JamzOhJamz@users.noreply.github.com>
2023-07-15 14:47:14 -0600

    Remove itemsSignature from api shop response

--- api/routes/shop.js
+++ api/routes/shop.js
@@ -4,10 +4,11 @@ export const prefix = '/api/shop';
 
 async function routes(app) {
     // TODO: Implement this route in the project. It currently mirrors the response from official server.
-    app.get('/available', async () => {
+    app.get('/available', async (req) => {
         try {
-            const response = await fetch('http://zombsroyale.io/api/shop/available');
+            const response = await fetch('http://zombsroyale.io/api/shop/available?' + new URLSearchParams(req.query).toString());
             const data = await response.json();
+            delete data.itemsSignature;
             return data;
         } catch (error) {
             return {
