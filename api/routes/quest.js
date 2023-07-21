6f12e8ae9aca41b033c0142f06fbb8b6a5af9e42
JamzOhJamz <87213748+JamzOhJamz@users.noreply.github.com>
2023-07-20 18:28:07 -0600

    Fixes

--- api/routes/quest.js
+++ api/routes/quest.js
@@ -4,9 +4,10 @@ export const prefix = '/api/quest';
 
 async function routes(app) {
     // TODO: Implement this route in the project. It currently mirrors the response from official server.
-    app.get('/available', async () => {
+    app.get('/available', async (req) => {
         try {
-            const response = await fetch('http://zombsroyale.io/api/quest/available');
+            delete req.query.userKey;
+            const response = await fetch('http://zombsroyale.io/api/quest/available?' + new URLSearchParams(req.query).toString());
             const data = await response.json();
             return data;
         } catch (error) {
--- api/routes/reward.js
+++ api/routes/reward.js
@@ -4,9 +4,10 @@ export const prefix = '/api/reward';
 
 async function routes(app) {
     // TODO: Implement this route in the project. It currently mirrors the response from official server.
-    app.get('/tracks', async () => {
+    app.get('/tracks', async (req) => {
         try {
-            const response = await fetch('http://zombsroyale.io/api/reward/tracks');
+            delete req.query.userKey;
+            const response = await fetch('http://zombsroyale.io/api/reward/tracks?' + new URLSearchParams(req.query).toString());
             const data = await response.json();
             return data;
         } catch (error) {
